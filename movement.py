6e633ecb5f6a618845e71035a8edfed74d2344e1
Vysybyl <kanthavel84@gmail.com>
2016-06-16 23:46:50 +0100

    Initial commit.

---
+++ movement.py
@@ -0,0 +1,24 @@
+
+
+
+class Movement(object):
+    def __init__(self):
+
+    def __init__(self, input_string):
+        self.raw_input = input_string
+        self.accelerometer = None
+        self.gyroscope = None
+        self.gravity = None
+        self.orientation = None
+
+        self.top = None
+        self.bottom = None
+        self.left = None
+        self.right = None
+        self.front = None
+        self.back = None
+
+        self.speed = None
+
+        
+
