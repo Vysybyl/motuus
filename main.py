8e3fd8789f9797589c8140cccc13d3fcf89f86f9
Vysybyl <kanthavel84@gmail.com>
2016-09-22 13:44:33 +0100

    Corrected main.py

--- main.py
+++ main.py
@@ -7,17 +7,17 @@ import imp
 
 def main():
     root_dir = os.path.dirname(os.path.abspath(__file__))
+    root_dir = os.path.dirname(root_dir)
     print root_dir
     site.addsitedir(root_dir)
     print "Launching Motuus!"
-    from motuus.web.home import run
-    packages = ["numpy", "pygame", "flask", "flask_socketio"]
+    packages = ["numpy", "pygame", "flask", "flask_socketio", "eventlet"]
     for p in packages:
         try:
             imp.find_module(p)
             print "Package Found: " + p
         except ImportError:
-            print "ERROR! Package Not Found: ", p, ". Please install the missing package"
+            print "ERROR! Package Not Found: ", p, "."
             return
     optional_packages = ["matplotlib", "panda3d"]
     for p in optional_packages:
@@ -26,7 +26,14 @@ def main():
             print "Package Found: " + p
         except ImportError:
             print "WARNING! Package Not Found: ", p, ". Some features will not work."
+            pip_message(p)
+    from motuus.web.home import run
     run()
+
+def pip_message(p):
+    print "Install the missing package using the command 'pip install " + p + "'"
+    print "Before that, you might need to upgrade pip using 'pip install --upgrade pip'"
+
 
 
 if __name__ == '__main__':
