6303facf31004549f0140c4009267b7cf18b19a0
Vysybyl <kanthavel84@gmail.com>
2016-09-29 22:30:03 +0100

    Added background image support.

--- motuus/visualize/screen.py
+++ motuus/visualize/screen.py
@@ -1,7 +1,8 @@
 import pygame
 from pygame.locals import *
+from os import path
+import constants
 
-from motuus.visualize.constants import *
 
 
 class Screen(object):
@@ -10,14 +11,23 @@ class Screen(object):
         pygame.init()  # initialize pygame engine
         self.__screen = pygame.display.set_mode((640, 480))
 
-    def display(self, background_color='black', fullscreen=False):
-        if type(background_color) is str:
-            c = Color(background_color)
-        else:
-            c = background_color
+    def display(self, background='black', fullscreen=False):
         if fullscreen:
             pygame.display.quit()
             self.__screen = pygame.display.set_mode((0, 0))
+        t = type(background)
+        if t is str:
+            if "." in background:
+                pat = path.join(constants.IMAGE_FOLDER, background)
+                img = pygame.image.load(pat)
+                self.__screen.blit(img, (0, 0))
+                pygame.display.flip()
+                return
+            else:
+                c = Color(background)
+        elif t is Color:
+            c = background
+
         self.__screen.fill(c)
         pygame.display.flip()
 
